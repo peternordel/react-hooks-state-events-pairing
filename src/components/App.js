834ea5c1348c4cd8c69415e510d3264ad494d9df
Peter Nordel <peternordel@gmail.com>
2023-04-14 12:45:18 -0600

    complete lab and all bonus deliverables (no styling)

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
 import video from "../data/video.js";
+import VideoCard from "./VideoCard";
+import React from "react";
 
 function App() {
-  console.log("Here's your data:", video);
+  const [videoData, setVideoData] = React.useState(video)
+
+  function handleRemove (index) {
+    setVideoData(prevVideoData => {
+      videoData.comments.splice(index, 1)
+      return {...prevVideoData}
+    })
+  }
+
+  function handleVotes (e) {
+    const {name} = e.target
+    setVideoData(prevVideoData => {
+      name === 'upvotes' ? prevVideoData.upvotes++ : prevVideoData.downvotes++
+      return {...prevVideoData}
+    })
+  }
 
   return (
     <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameBorder="0"
-        allowFullScreen
-        title="Thinking in React"
-      />
+      <VideoCard videoData={videoData} handleClick={handleVotes} handleRemove={handleRemove} />
     </div>
   );
 }
