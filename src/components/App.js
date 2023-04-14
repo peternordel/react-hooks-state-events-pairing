76217adeb5a1aae6a4ff908369b70da8e913a8d8
Peter Nordel <peternordel@gmail.com>
2023-04-14 13:44:55 -0600

    clean up code

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,10 @@ import video from "../data/video.js";
 import VideoCard from "./VideoCard";
 import React from "react";
 
-function App() {
-  const [videoData, setVideoData] = React.useState(video)
-
-  function handleRemove (index) {
-    setVideoData(prevVideoData => {
-      videoData.comments.splice(index, 1)
-      return {...prevVideoData}
-    })
-  }
-
-  function handleVotes (e) {
-    const {name} = e.target
-    setVideoData(prevVideoData => {
-      name === 'upvotes' ? prevVideoData.upvotes++ : prevVideoData.downvotes++
-      return {...prevVideoData}
-    })
-  }
-
+export default function App() {
   return (
     <div className="App">
-      <VideoCard videoData={videoData} handleClick={handleVotes} handleRemove={handleRemove} />
+      <VideoCard video={video} />
     </div>
   );
 }
-
-export default App;
